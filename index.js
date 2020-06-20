bd3f8d439764cdc329919eb9e2aeffe5c49815bb
YovSlava <50184380+YovSlava@users.noreply.github.com>
2020-06-20 19:13:25 +0300

    20.06

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+var word = 'bottles';
+var count = 99;
+count -= 1;
+number = number - 1;
+if (count < 0) {
+ ducument.write(number + " " + word + " of beer on the wall");
+ ducument.write(number + " " + word + " of beer,");
+ ducument.write("Take one down, pass it around,");
+}
+if (number > 0) {
+ ducument.write(number + " " + word + " of beer on the wall.");
+} else {
+ ducument.write("No more " + word + " of beer on the wall.");
+}
