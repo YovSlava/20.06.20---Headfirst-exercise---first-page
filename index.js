43d049ba42bb585e67735408de65960c89255534
YovSlava <50184380+YovSlava@users.noreply.github.com>
2020-06-22 22:28:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,24 +5,28 @@ var guess;
 var hits = 0;
 var guesses = 0;
 var isSunk = false;
+
+
 while (isSunk == false) {
- guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+ var guess = prompt('Pick a number between 0 and 6!')
  if (guess < 0 || guess > 6) {
-  alert("Please enter a valid cell number!");
+  var guesses = alert('Pick a number between 0 and 6, please!')
  } else {
   guesses = guesses + 1;
-  if (guess == location1 || guess == location2 || guess == location3) {
-   alert("HIT!");
-   hits = hits + 1;
-   if (hits == 3) {
-    isSunk = true;
-    alert("You sank my battleship!");
-   }
+  if (guess == location1 || guess == location2 || guess == location3)
+   alert('Hit!');
+  hits = hits + 1;
+  if (hits == 3) {
+   isSunk = true;
+   alert('You have sunk my battleship!')
   } else {
-   alert("MISS");
+   alert('Miss!')
   }
+  var stats = "You took " + guesses + " guesses to sink the battleship, " +
+   "which means your shooting accuracy was " + (3 / guesses);
+  alert(stats);
+
+
  }
+
 }
-var stats = "You took " + guesses + " guesses to sink the battleship, " +
- "which means your shooting accuracy was " + (3 / guesses);
-alert(stats);
