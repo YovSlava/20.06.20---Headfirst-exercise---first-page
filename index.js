4a0d00c1768b99def9cc176139d4f8efb815feb3
YovSlava <50184380+YovSlava@users.noreply.github.com>
2020-06-23 10:42:09 +0300

    battleship update

--- index.js
+++ index.js
@@ -6,4 +6,25 @@ var hits = 0;
 var guesses = 0;
 var isSunk = false;
 
+while (isSunk == false) {
+ guess = prompt('Choose a valid number between 0 and 6!');
+ if (guess < 0 || guess > 6) {
+  alert('Choose a valid number between 0 and 6, please!');
+ } else {
+  guesses = guesses + 1;
+  if (guess == location1 || guess == location2 || guess == location3) {
+   alert('Hit!');
+   hits = hits + 1;
+   if (hits == 3) {
+    isSunk = true;
+    alert('You have sunk my battleship!');
+   }
 
+  }
+  else {
+   alert('Miss!')
+  }
+
+ }
+
+}
