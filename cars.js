8c2851e188dba79012210c474dea7724401852e6
YovSlava <50184380+YovSlava@users.noreply.github.com>
2020-08-12 23:07:22 +0300

    Create cars.js

---
+++ cars.js
@@ -0,0 +1,16 @@
+var taxi = {
+ name: 'Taxi',
+ modelNew: false,
+ year: 1950,
+ color: 'yellow',
+ isLicenced: true
+};
+
+function isToBeConsidered(taxi) {
+ if (taxi.year > 1965) {
+  return false
+ } else if (isLicenced = false) {
+  return false
+ }
+ return true;
+}
