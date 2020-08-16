abc9e24a512a05fbd237cf5bbd87980e9840dfe4
YovSlava <50184380+YovSlava@users.noreply.github.com>
2020-08-16 22:55:29 +0300

    Update music.js

--- music.js
+++ music.js
@@ -2,12 +2,17 @@ var music = {
  name: 'Aloha',
  artist: 'Mariah',
  year: 1986,
- isPlaying: false,
- post50sMusic: function() {
-  if(this.year > 1950) {
-  this.isPlaying = true;
-  } else {
-   this.isPlaying = false;
+ playing: false,
+ retro: function () {
+  if (!this.playing) {
+   this.playing = true;
+   console.log('This retro hit is ' + this.name + ' by ' + this.artist)
+  }
+ },
+ modern: function () {
+  if (this.playing) {
+   this.playing = false;
   }
  }
 };
+music.retro();
