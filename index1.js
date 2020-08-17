7ac83518cee7939806e02002841f1501a52322fc
YovSlava <50184380+YovSlava@users.noreply.github.com>
2020-08-17 22:57:05 +0300

    Create index1.js

---
+++ index1.js
@@ -0,0 +1,22 @@
+var output = [];
+
+var i = 0;
+
+function FizzBuzz() {
+ for (i = 0; i <= 300; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+   console.log(output.push('FizzBuzz'));
+  } else if (i % 3 === 0) {
+   console.log(output.push('Fizz'));
+  } else if (i % 5 === 0) {
+   console.log(output.push('Buzz'));
+  } else {
+   console.log(output.push(i));
+  }
+ }
+
+}
+
+FizzBuzz();
+console.log(output);
+
