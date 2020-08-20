9c72fcd728cef3c60a901011dca33d69f7417ad7
YovSlava <50184380+YovSlava@users.noreply.github.com>
2020-08-20 22:15:15 +0300

    commit

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
-var guess;
-var hits = 0;
-var guesses = 0;
-var isSunk = false;
+var myLinkCollection = document.getElementsByTagName('P');
 
-while (isSunk == false) {
-  guess = prompt("Choose a valid number between 0 and 6!");
-  if (guess < 0 || guess > 6) {
-    alert("Choose a valid number between 0 and 6, please!");
-  } else {
-    guesses = guesses + 1;
-    if (guess == location1 || guess == location2 || guess == location3) {
-      alert("Hit!");
-      hits = hits + 1;
-      if (hits == 3) {
-        isSunk = true;
-        alert("You have sunk my battleship!");
-      }
-    } else {
-      alert("Miss!");
+for (i = 0; i < myLinkCollection.length; i++) {
+  if (myLinkCollection[i].getElementByClassName == 'linkCollection') {
+    myLinkCollection[i].onclick = funtion() {
+      this.style.color = '#f00';
     }
   }
-}
-
-
+};
