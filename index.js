a0fb6da4aae3406958188637e502db365f046265
YovSlava <50184380+YovSlava@users.noreply.github.com>
2020-08-23 15:08:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function Housekeeper (name, age, experience) {
-  this.name = name
-  this.age = age
-  this.experience = experience
+function Housekeeper(name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+
+var test10 = null;
+console.log(test10);
+console.log(typeof test10);
+
+var num = 99;
+if (num == "99") {
+  console.log("wow");
+} else {
+  console.log("not happening");
 }
