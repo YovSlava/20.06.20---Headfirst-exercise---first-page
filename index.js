e78343467719e1be191ef54a2658a0763b0defd8
YovSlava <50184380+YovSlava@users.noreply.github.com>
2020-08-25 22:14:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ var test10 = null;
 console.log(test10);
 console.log(typeof test10);
 
+
+function Duck(sound) {
+this.sound = sound;
+this.quack = function() {console.log(this.sound);}
+}
+var toy = new Duck("quack quack");
+toy.quack();
+console.log(typeof toy);
+console.log(toy instanceof Duck);
