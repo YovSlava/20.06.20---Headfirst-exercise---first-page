1b70d08926ecef73234e6453d9ea7fae164c2555
YovSlava <50184380+YovSlava@users.noreply.github.com>
2020-08-29 15:55:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ console.log(typeof toy);
 console.log(toy instanceof Duck);
 
 
-
+// //
 
 function factorial(n) {
 for (var i=n-1; i>0; i--) {
@@ -27,4 +27,29 @@ for (var i=n-1; i>0; i--) {
 }
 return n
 
+}
+
+
+// // 
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    var r = readLine();
+
+    const PI = Math.PI; 
+
+    const area = PI * Math.pow(r, 2); 
+
+    const perimeter = 2 * PI * r; 
+    
+    console.log(area);
+    console.log(perimeter);
+
+    try {    
+        // Attempt to redefine the value of constant variable PI
+        PI = 0;
+        // Attempt to print the value of PI
+        console.log(PI);
+    } catch(error) {
+        console.error("You correctly declared 'PI' as a constant.");
+    }
 }
